e1466e52389abd6fc692083f6f206aa1137b1026
Rishabh Bajpai <rishabhbajpai976@gmail.com>
2025-09-22 10:51:39 +0530

    Taking User Input

---
+++ Python Basic Codes/Taking_Input_From_User.py
@@ -0,0 +1,11 @@
+# Taking Input From User
+name = input("Enter your name: ")
+print("Hello, " + name + "! Welcome to the Python world.")
+age = input("Enter your age: ")
+print("You are " + age + " years old.")
+height = input("Enter your height in cm: ")
+print("You are " + height + " cm tall.")
+weight = input("Enter your weight in kg: ")
+print("You weigh " + weight + " kg.")
+# Note: The input() function always returns a string.
+#  If you need a different data type, you can convert it using int(), float(), etc.
